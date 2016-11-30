8f787ab62189ae2a916869f9f6c8aea08882f53f
daniiltserin <daniilborowkow@yandex.ru>
2016-11-30 22:23:54 +0600

    Научился использовать конструкции типа #{}, number.times{|i|}

--- 4.rb
+++ 4.rb
@@ -1,11 +1,17 @@
-puts 'enter x'
-x = gets
-x = x.to_i
-puts 'enter y'
-y = gets
-y = y.to_i
-puts 'enter z'
-z = gets
-z = z.to_i
-sum = x + y + z
-puts "Sum of all vars is: #{sum}"
+# puts 'введите x'
+# x = gets
+# x = x.to_i
+# puts 'enter y'
+# y = gets
+# y = y.to_i
+# puts 'enter z'
+# z = gets
+# z = z.to_i
+# sum = x + y + z
+# # puts "Sum of all vars is: #{sum}"
+
+puts 'Enter string'
+str = gets
+puts 'enter number'
+n = gets.to_i
+n.times{|i| puts "#{str} , #{i.to_s}"}
